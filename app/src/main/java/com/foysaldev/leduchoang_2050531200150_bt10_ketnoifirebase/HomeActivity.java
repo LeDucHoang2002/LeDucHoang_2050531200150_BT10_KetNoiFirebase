0de9bee84e96d332110c5fa04f0aa64e2258a3d6
LeDucHoang2002 <leduchoang.10022002@gmail.com>
2022-11-08 08:42:58 +0700

    SingIn SignUp

---
+++ app/src/main/java/com/foysaldev/leduchoang_2050531200150_bt10_ketnoifirebase/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.foysaldev.leduchoang_2050531200150_bt10_ketnoifirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
